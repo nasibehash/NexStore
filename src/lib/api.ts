a63962e7473bb7623a48f4727b5e3d43f25c730e
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2025-04-12 11:37:27 +0330

    Feature: Add promise type

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,12 @@
-export async function fetchProducts():  Promise<string> {
+import { Product } from '@/types/product.type';
+
+export async function fetchProducts():  Promise<Product[]> {
   const res = await fetch('https://fakestoreapi.com/products');
   if (!res.ok) throw new Error('Failed to fetch products');
   return res.json();
 }
 
-export async function fetchProductById(id: string): Promise<string> {
+export async function fetchProductById(id: string): Promise<any> {
   const res = await fetch(`https://fakestoreapi.com/products/${id}`);
   if (!res.ok) throw new Error('Failed to fetch product');
   return res.json();
