2e6cdae27609c1fa0bc58ebe0e9292752b9249a5
n.ashtari <n.ashtari@mydigipay.com>
2025-04-24 22:10:15 +0330

    Feature: Add title and description filter to product list

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link';
-import { Product } from '@/types/product.type';
+import {Product} from '@/types/product.type';
 
 export default function ProductCard({product}: { product: Product }) {
-  return (
-    <div className="border rounded-lg p-4 shadow-sm hover:shadow-md transition">
-      <Link href={`/product/${product.id}`}>
-        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden">
-          <img
-            src={product.image}
-            alt={product.title}
-            className="object-contain h-40 w-full"
-          />
+    return (
+        <div className="border rounded-lg p-4 shadow-sm hover:shadow-md transition">
+            <Link href={`/product/${product.id}`}>
+                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden">
+                    <img
+                        src={product.image}
+                        alt={product.title}
+                        className="object-contain h-40 w-full"
+                    />
+                </div>
+                <h3 className="mt-2 font-semibold text-sm line-clamp-2">{product.title}</h3>
+                <h3 className="mt-2 font-semibold text-sm line-clamp-2">{product.description}</h3>
+                <p className="text-gray-700 font-bold mt-1">${product.price}</p>
+            </Link>
         </div>
-        <h3 className="mt-2 font-semibold text-sm line-clamp-2">{product.title}</h3>
-        <p className="text-gray-700 font-bold mt-1">${product.price}</p>
-      </Link>
-    </div>
-  );
+    );
 }
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,13 +1,36 @@
+'use client';
+
+import {useState} from 'react';
+
 import ProductCard from './ProductCard';
 
-import { Product } from '@/types/product.type';
+import {Product} from '@/types/product.type';
 
 export default function ProductList({products}: { products: Product[] }) {
-  return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product}/>
-      ))}
-    </div>
-  );
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const filteredProducts = products.filter((product: Product) => {
+        return product.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            product.description.toLowerCase().includes(searchQuery.toLowerCase());
+    });
+
+    return (
+        <>
+            <input
+                type="text"
+                className="w-full p-2 border rounded mb-4"
+                placeholder="Search..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {filteredProducts.map((product) => (
+                    <ProductCard key={product.id} product={product}/>
+                ))}
+            </div>
+            {filteredProducts.length === 0 && (
+                <p className="text-center text-gray-500 col-span-full mt-8">Not found!</p>
+            )}
+        </>
+    );
 }
